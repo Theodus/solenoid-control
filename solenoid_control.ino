1f572b70f58a1acd83b2d38c11c75b580b4558c2
Theo Butler <theodusbutler@gmail.com>
2017-05-24 14:21:33 -0400

    rotational control

--- solenoid_control.ino
+++ solenoid_control.ino
@@ -4,6 +4,8 @@
 // Launchers & Targets //
 /////////////////////////
 
+// TODO ammunition aware?
+
 typedef struct {
   byte pin; // control pin
   byte dly; // delay between high and low signals
@@ -19,30 +21,56 @@ Launcher launcher24V = {
   dly: 30 // TODO tweak delay
 };
 
-/* TODO real logic:
-
-- angle between targets = 25 (about)
-- determine which solenoid to use in order to minimize delay for targeting
-  - also ammunition count for each solenoid
-- per-target delay time
-
-*/
 Servo servo;
-const unsigned int per_target_delay = 50; // TODO tweak delay
-const byte rotations[2] = {20, 45};
-byte last_target = 0;
+const unsigned int per_target_delay = 100; // TODO tweak delay
+const byte angle_between_targets = 13; // TODO verify
+const byte pivot = 3; // TODO tweak pivot based on requirements
+const byte launcher24V_offset = 2; // 3?
+byte last_target = pivot; // position of solenoid36V
 
+// [0] [1] [2] [3] [4] [5] [6] [7]
+
+// both solenoids must always be aiming at a target on completion
 void aim(byte target) {
-  if (target != last_target) {
-    servo.write(rotations[target]);
-    delay(per_target_delay * abs(target - last_target));
-    last_target = target;
+  Serial.println(target);
+  byte pos36V = last_target;
+  byte pos24V = last_target + launcher24V_offset;
+  if (target == pos36V) { launch(launcher36V); }
+  else if (target == pos24V) { launch(launcher24V); } 
+  else {
+    if (target < pos36V) { aim_and_launch36V(target); }
+    else if (target > pos24V) { aim_and_launch24V(target); }
+    else {
+      // between the launchers (favor movements toward pivot)
+      if (pos36V >= pivot) { aim_and_launch24V(target); }
+      else { aim_and_launch36V(target); }      
+    }
   }
 }
 
-void launch(Launcher l, byte target) {
-  aim(target);
-  //servo.write(rotations[target]);
+void aim_and_launch36V(byte i) {
+  rotate_to(i);
+  launch(launcher36V);
+}
+
+void aim_and_launch24V(byte i) {
+  
+  rotate_to(i - launcher24V_offset);
+  launch(launcher24V);
+}
+
+// rotate launcher36V to target at given index
+void rotate_to(byte i) {
+  Serial.print(i);
+  Serial.print("\t");
+  Serial.print(i * angle_between_targets);
+  Serial.println();
+  servo.write(i * angle_between_targets);
+  delay(per_target_delay * abs(i - last_target));
+  last_target = i;
+}
+
+void launch(Launcher l) {
   digitalWrite(l.pin, HIGH);
   delay(l.dly);
   digitalWrite(l.pin, LOW);
@@ -64,6 +92,8 @@ void setup() {
   servo.attach(4);
   aim(last_target);
 
+  Serial.begin(9600);
+
   blink(3);
 }
 
@@ -74,12 +104,14 @@ void handleNoteOn(byte channel, byte note, byte velocity) {
   // TODO channel
   // velocity is ignored
   switch (note) {
-    // C1
-    case 24: launch(launcher36V, 0); break;
-    // D1
-    case 26: launch(launcher24V, 0); break;
-    // E1
-    case 28: launch(launcher24V, 1); break;
+    case 24: aim(0); break; // C0
+    case 26: aim(1); break; // D0
+    case 28: aim(2); break; // E0
+    case 29: aim(3); break; // F0
+    case 31: aim(4); break; // G0
+    case 33: aim(5); break; // A0
+    case 35: aim(6); break; // B0
+    case 36: aim(7); break; // C1
   }
 }
 
