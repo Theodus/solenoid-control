fb75237e0ebf56caf34ec6fca316e582415260ad
Theo Butler <theodusbutler@gmail.com>
2017-05-25 12:09:21 -0400

    cleanup after demo

--- solenoid_control.ino
+++ solenoid_control.ino
@@ -28,12 +28,9 @@ const byte pivot = 3; // <- // TODO tweak pivot based on requirements
 const byte launcher24V_offset = 2;
 byte last_target = pivot; // position of solenoid36V
 
-// [0] [1] [2] [3] [4] [5] [6] [7]
-
 // both solenoids must always be aiming at a target on completion
 // all rotations are based on the position of the 36V launcher
-void aim(byte target) {
-  //Serial.println(target);
+void aim_and_launch(byte target) {
   byte pos36V = last_target;
   byte pos24V = last_target + launcher24V_offset;
   if (target == pos36V) { launch(launcher36V); }
@@ -41,7 +38,7 @@ void aim(byte target) {
   else if (target < pos36V) { aim_and_launch36V(target); }
   else if (target > pos24V) { aim_and_launch24V(target); }
   else {
-    // between the launchers (favor movements toward pivot)
+    // target between the launchers (favor movements toward pivot)
     if (pos36V >= pivot) { aim_and_launch24V(target); }
     else { aim_and_launch36V(target); }      
   }
@@ -59,10 +56,6 @@ void aim_and_launch24V(byte i) {
 
 // rotate launcher36V to target at given index
 void rotate_to(byte i) {
-  /*Serial.print(i);
-  Serial.print("\t");
-  Serial.print(i * angle_between_targets);
-  Serial.println();*/
   servo.write(i * angle_between_targets);
   delay(per_target_delay * abs(i - last_target));
   last_target = i;
@@ -90,30 +83,29 @@ void setup() {
   servo.attach(4);
   rotate_to(last_target);
 
-  //Serial.begin(9600);
-
   blink(3);
 }
 
 void loop() { usbMIDI.read(); } // Do not modify!
 
 void handleNoteOn(byte channel, byte note, byte velocity) {
-  digitalWrite(LED_BUILTIN, HIGH);
   if (channel != 6) { return; }
+  digitalWrite(LED_BUILTIN, HIGH);
   // velocity is ignored
   switch (note) {
-    case 24: aim(0); break; // C0
-    case 26: aim(1); break; // D0
-    case 28: aim(2); break; // E0
-    case 29: aim(3); break; // F0
-    case 31: aim(4); break; // G0
-    case 33: aim(5); break; // A0
-    case 35: aim(6); break; // B0
-    case 36: aim(7); break; // C1
+    case 24: aim_and_launch(0); break; // C0
+    case 26: aim_and_launch(1); break; // D0
+    case 28: aim_and_launch(2); break; // E0
+    case 29: aim_and_launch(3); break; // F0
+    case 31: aim_and_launch(4); break; // G0
+    case 33: aim_and_launch(5); break; // A0
+    case 35: aim_and_launch(6); break; // B0
+    case 36: aim_and_launch(7); break; // C1
   }
 }
 
 void handleNoteOff(byte channel, byte note, byte velocity) {
+  if (channel != 6) { return; }
   digitalWrite(LED_BUILTIN, LOW);
   // Nothing done
 }
